c4c787ab50417e31383e3aecd0cdd4b9a6024f03
gokulg <gk.is.a.techie@gmail.com>
2024-10-21 11:54:07 +0530

    Added an api to pick item(s) at random from a given collection

--- src/main/java/org/gk2k23/controllers/Controller.java
+++ src/main/java/org/gk2k23/controllers/Controller.java
@@ -2,18 +2,20 @@ package org.gk2k23.controllers;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.*;
 import jakarta.inject.Inject;
-import org.gk2k23.models.CollectionEntity;
-import org.gk2k23.models.NewCollectionRequest;
-import org.gk2k23.models.Response;
-import org.gk2k23.models.ResponseStatus;
+import org.gk2k23.models.*;
 import org.gk2k23.repository.CollectionRepository;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @io.micronaut.http.annotation.Controller("/api")
 public class Controller {
+
+    private static final Random random = new SecureRandom();
 
     @Inject
     private CollectionRepository repository;
@@ -27,5 +29,32 @@ public class Controller {
     public HttpResponse<Response> saveCollection(@Body NewCollectionRequest request) {
         CollectionEntity entity = repository.save(new CollectionEntity(request));
         return HttpResponse.ok(new Response(ResponseStatus.SUCCESS.name(), "Collection saved successfully", entity.getId()));
+    }
+
+    @Get(uri = "pickAtRandom/{collectionName}", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Response> pickAtRandom(@PathVariable String collectionName, @QueryValue(defaultValue = "1") Integer count) {
+        CollectionEntity entity = repository.findByCollectionName(collectionName).stream().findFirst().orElse(null);
+        if (entity == null) {
+            return HttpResponse.badRequest(new Response(ResponseStatus.FAILURE.name(), "Collection not found for given name"));
+        }
+        List<String> results = new ArrayList<>();
+        while (count > 0 && !entity.getCollection().getItems().isEmpty()) {
+            results.add(pickAnItemAtRandom(entity.getCollection().getItems(), collectionName));
+            count--;
+        }
+        return HttpResponse.ok(new Response(ResponseStatus.SUCCESS.name(), "Successfully picked items at random", results));
+    }
+
+    private String pickAnItemAtRandom(List<Item> items, String pathName) {
+        Item item = items.get(random.nextInt(items.size()));
+        if (item.getChildren() == null || item.getChildren().isEmpty()) {
+            items.remove(item);
+            return pathName + "/" + item.getName();
+        } else if(item.getChildren().size() == 1) {
+            items.remove(item);
+            return pathName + "/" + item.getName() + "/" + item.getChildren().get(0).getName();
+        } else {
+            return pickAnItemAtRandom(item.getChildren(), pathName + "/" + item.getName());
+        }
     }
 }
--- src/main/java/org/gk2k23/repository/CollectionRepository.java
+++ src/main/java/org/gk2k23/repository/CollectionRepository.java
@@ -1,10 +1,15 @@
 package org.gk2k23.repository;
 
+import io.micronaut.data.mongodb.annotation.MongoFindQuery;
 import io.micronaut.data.mongodb.annotation.MongoRepository;
 import io.micronaut.data.repository.CrudRepository;
 import org.gk2k23.models.CollectionEntity;
 
+import java.util.List;
+
 @MongoRepository
 public interface CollectionRepository extends CrudRepository<CollectionEntity, String> {
 
+    @MongoFindQuery("{ 'collection.name': :name }")
+    List<CollectionEntity> findByCollectionName(String name);
 }
